d5a648722b85aecbb8ac4f79510896fa73b27720
dima-dot-git <61382868+dima-dot-git@users.noreply.github.com>
2024-04-07 20:03:16 +0300

    Add files via upload

--- website/settings.py
+++ website/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'gallery',
-    # 'crispy_forms',
-    # "crispy_bootstrap3",
+    'crispy_forms',
+    "crispy_bootstrap3",
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap3"
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_ROOT = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
